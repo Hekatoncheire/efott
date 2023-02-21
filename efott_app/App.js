76312b1d31ac4fbe8dd1a6f7ebb0e61e2f84887d
GyureszBa <gyorgy.dezsenyi@gmail.com>
2023-02-21 11:19:16 +0100

    home tab navigator created

--- efott_app/App.js
+++ efott_app/App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
 import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native' ;
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons'
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen(){
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+function CalendarScreen(){
+  return (
+    <View>
+      <Text>Calendar</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeScreen}/>
+        <Tab.Screen name='Calendar' component={CalendarScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
